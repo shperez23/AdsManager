344ace4c15f8536454b1ebe8ca2897d385e80180
Sergio Perez <67597899+shperez23@users.noreply.github.com>
2026-03-17 19:15:39 -0500

    feat(auth): implementar flujo end-to-end con guard e interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,14 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { errorInterceptor } from './core/api/interceptors/error.interceptor';
+import { authInterceptor } from './core/auth/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(appRoutes),
-    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor, authInterceptor])),
     provideClientHydration(withEventReplay()),
   ],
 };
